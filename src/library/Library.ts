a322edc91d0d6b1158f35fb3420c2b3c10499945
Berenguer Pou <berenguer.pou@gmail.com>
2024-03-10 18:27:02 +0100

    add find book method and tests

--- src/library/Library.ts
+++ src/library/Library.ts
@@ -11,4 +11,22 @@ export class Library {
   getAllBooks() {
     return this.#books;
   }
+
+  getBookByTitle(title: string) {
+    if (this.#books.length === 0) {
+      throw new Error("The library is empty");
+    }
+
+    const foundBook = this.#books.find((book) => book.title === title);
+
+    if (foundBook === null) {
+      throw new Error("Error finding book");
+    }
+
+    if (foundBook === undefined) {
+      throw new Error("Book not found");
+    }
+
+    return foundBook;
+  }
 }
